e5cf505a2a13354a8b29dfb056a9aa4ac402fda1
Lucie Bazantova <luciebaz@email.cz>
2021-06-24 21:05:24 +0200

    zaloha pred resenim splitu sloupcu excel/csv

--- src/main/java/CodeApp.java
+++ src/main/java/CodeApp.java
@@ -8,28 +8,37 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 public class CodeApp {
-    public static void main (String args[]) throws IOException {
+    public static void main(String[] args) throws IOException {
 
 
-String csvFilePath="C:\\Users\\lucie\\OneDrive\\Plocha\\num2.csv";
+        String csvFilePath = "C:\\Users\\lucie\\OneDrive\\Plocha\\num2.csv";
 
         try {
             BufferedReader reader = new BufferedReader(new FileReader(csvFilePath));
-            CSVParser records = CSVParser.parse(reader, CSVFormat.EXCEL.withFirstRecordAsHeader().withIgnoreHeaderCase().withTrim());
+            CSVParser records = CSVParser.parse(reader, CSVFormat.EXCEL);
             Map<String, CodeConverter> codeMap = new HashMap<String, CodeConverter>();
             for (CSVRecord record : records) {
-             CodeConverter codeConverter = new CodeConverter();
-             codeConverter.setCode2(record.get(0));
-             codeConverter.setCode3(record.get(1));
+                CodeConverter codeConverter = new CodeConverter();
+                codeConverter.setPairOfCodes(record.get(0));
 
-             codeMap.put(record.get(0), codeConverter);
-             System.out.println(codeMap);
+                codeMap.put(record.get(0), codeConverter);
+
             }
+//            System.out.println(codeMap);
 
+           Collection collection = codeMap.values();
+           Iterator iterator = collection.iterator();
+           while (iterator.hasNext()!=false) {
+               CodeConverter codeConverter = (CodeConverter) iterator.next();
+
+           }
+            System.out.println(("Your code is: ") + codeMap.get("GD"));
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
--- src/main/java/CodeConverter.java
+++ src/main/java/CodeConverter.java
@@ -1,29 +1,35 @@
 public class CodeConverter {
 
-    private String code2;
-    private String code3;
+//    private String code2;
+//    private String code3;
+    private String pairOfCodes;
 
-    public String getCode2() {
-        return code2;
+    public String getPairOfCodes() {
+        return pairOfCodes;
     }
 
-    public void setCode2(String code2) {
-        this.code2 = code2;
+    public void setPairOfCodes(String pairOfCodes) {
+        this.pairOfCodes = pairOfCodes;
     }
 
-    public String getCode3() {
-        return code3;
-    }
-
-    public void setCode3(String code3) {
-        this.code3 = code3;
-    }
+//    public String getCode2() {
+//        return code2;
+//    }
+//
+//    public void setCode2(String code2) {
+//        this.code2 = code2;
+//    }
+//
+//    public String getCode3() {
+//        return code3;
+//    }
+//
+//    public void setCode3(String code3) {
+//        this.code3 = code3;
+//    }
 
     @Override
     public String toString() {
-        return "CodeConverter{" +
-                "code2='" + code2 + '\'' +
-                ", code3='" + code3 + '\'' +
-                '}';
+        return pairOfCodes;
     }
 }
